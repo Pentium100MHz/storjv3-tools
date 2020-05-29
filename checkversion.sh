7f43ba8c75e206d49bdf8072905e43f024692de3
Pentium100MHz <Pentium100MHz@users.noreply.github.com>
2020-05-29 11:21:34 +0300

    Create checkversion.sh

---
+++ checkversion.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+t=`/usr/bin/curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:storjlabs/storagenode:pull" | cut -d'"' -f 4`
+/usr/bin/curl -s -H "Authorization: Bearer $t" https://index.docker.io/v2/storjlabs/storagenode/manifests/beta | jq '.history[0].v1Compatibility | fromjson |.created'

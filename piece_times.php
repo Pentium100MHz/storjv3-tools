d7e966b68ef965f66572784e8b627a78c87c1a5c
Pentium100MHz <Pentium100MHz@users.noreply.github.com>
2021-11-17 14:44:58 +0200

    Update piece_times.php

--- piece_times.php
+++ piece_times.php
@@ -1,45 +1,57 @@
 <?php
 //usage: cat /path/to/log/file.log | php piece_times.php
+
+function formatDate($timestamp) {
+//produces date in a format that Storj v3 node uses in its logs
+return sprintf("%s.%dZ", gmdate('Y-m-d\TH:i:s',$timestamp), ($timestamp-floor($timestamp))*1000);
+}
+
 $log=array();
 exec("cat | sed 's/\x1b\[[0-9;]*m//g' ",$log);
 $max=0; $min=PHP_FLOAT_MAX;
 foreach ($log as $line) {
         $parts=explode("\t",$line);
-        $json=json_decode($parts[4],true);
-        $action=$parts[3];
-        switch ($action) {
-        case "download started":
-                        if (isset($pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]])) {
-                                $pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]++;
-                        } else {
-                                $pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]=1;
-                        }
-                        $times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][]=$data=date_format(date_create_from_format('Y-m-d?H:i:s.uT',$parts[0]),"U.u");
-                        break;
-        case "downloaded":
-        case "download failed":
-        case "download canceled":
-                        if (isset($pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]])) {
-                                if ($pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]] > 0) {
-                                        $pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]--;
-                                        $endtime=date_format(date_create_from_format('Y-m-d?H:i:s.uT',$parts[0]),"U.u");
-                                        $duration=$endtime-$times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][0];
-                                        unset($times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][0]);
-                                        $times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]=array_values($times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]);
-                                        //printf("%s %s %.3f\n", $json["Satellite ID"],$json["Piece ID"],$duration);
-                                        if ( $duration > $max) {
-                                                $max = $duration;
-                                                $max_piece=$json;
-                                        }
-                                        if ( $duration < $min) {
-                                                $min = $duration;
-                                                $min_piece=$json;
+        if (isset($parts[4])) {
+                $json=json_decode($parts[4],true);
+                $action=$parts[3];
+                switch ($action) {
+                case "download started":
+                                if (isset($pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]])) {
+                                        $pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]++;
+                                } else {
+                                        $pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]=1;
+                                }
+                                $times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][]=$data=date_format(date_create_from_format('Y-m-d?H:i:s.uT',$parts[0]),"U.u");
+                                break;
+                case "downloaded":
+                case "download failed":
+                case "download canceled":
+                                if (isset($pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]])) {
+                                        if ($pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]] > 0) {
+                                                $pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]--;
+                                                $endtime=date_format(date_create_from_format('Y-m-d?H:i:s.uT',$parts[0]),"U.u");
+                                                $duration=$endtime-$times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][0];
+                                                //printf("%s %s %.3f\n", $json["Satellite ID"],$json["Piece ID"],$duration);
+                                                if ( $duration > $max) {
+                                                        $max = $duration;
+                                                        $max_piece=$json;
+                                                        $max_start=$times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][0];
+                                                        $max_end=$endtime;
+                                                }
+                                                if ( $duration < $min) {
+                                                        $min = $duration;
+                                                        $min_piece=$json;
+                                                        $min_start=$times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][0];
+                                                        $min_end=$endtime;
+                                                }
+                                                unset($times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][0]);
+                                                $times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]=array_values($times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]);
                                         }
                                 }
-                        }
-                        break;
+                                break;
                 } //switch
+        } //if
 } //foreach
 
-printf("Slowest: satellite: %s, piece: %s, time: %.3f\n", $max_piece["Satellite ID"], $max_piece["Piece ID"], $max);
-printf("Fastest: satellite: %s, piece: %s, time: %.3f\n", $min_piece["Satellite ID"], $min_piece["Piece ID"], $min);
+printf("Slowest: satellite: %s, piece: %s, time: %.3f start: %s, end: %s\n", $max_piece["Satellite ID"], $max_piece["Piece ID"], $max, formatDate($max_start),formatDate($max_end));
+printf("Fastest: satellite: %s, piece: %s, time: %.3f start: %s, end: %s\n", $min_piece["Satellite ID"], $min_piece["Piece ID"], $min, formatDate($min_start),formatDate($min_end));

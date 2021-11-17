e7bdf8bfa065209b97bacdd11a8a6bc895aba926
Pentium100MHz <Pentium100MHz@users.noreply.github.com>
2021-11-17 21:55:02 +0200

    Update piece_times.php

--- piece_times.php
+++ piece_times.php
@@ -9,7 +9,9 @@ return sprintf("%s.%03dZ", gmdate('Y-m-d\TH:i:s',$timestamp), ($timestamp-floor(
 $log=array();
 exec("cat | sed 's/\x1b\[[0-9;]*m//g' ",$log);
 $max=0; $min=PHP_FLOAT_MAX;
+$sum_down=0; $count_down=0;
 foreach ($log as $line) {
+//      printf("%s\n", $line);
         $parts=explode("\t",$line);
         if (isset($parts[4])) {
                 $json=json_decode($parts[4],true);
@@ -31,7 +33,8 @@ foreach ($log as $line) {
                                                 $pieces[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]]--;
                                                 $endtime=date_format(date_create_from_format('Y-m-d?H:i:s.uT',$parts[0]),"U.u");
                                                 $duration=$endtime-$times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][0];
-                                                //printf("%s %s %.3f\n", $json["Satellite ID"],$json["Piece ID"],$duration);
+                                                $count_down++; $sum_down+=$duration;
+                                                //printf("%s %s %.3f %s %s\n", $json["Satellite ID"],$json["Piece ID"],$duration, formatDate($times[$json["Satellite ID"]][$json["Piece ID"]][$json["Action"]][0]), formatDate($endtime));
                                                 if ( $duration > $max) {
                                                         $max = $duration;
                                                         $max_piece=$json;
@@ -53,5 +56,10 @@ foreach ($log as $line) {
         } //if
 } //foreach
 
-printf("Slowest: satellite: %s, piece: %s, time: %.3f, start: %s, end: %s\n", $max_piece["Satellite ID"], $max_piece["Piece ID"], $max, formatDate($max_start),formatDate($max_end));
-printf("Fastest: satellite: %s, piece: %s, time: %.3f, start: %s, end: %s\n", $min_piece["Satellite ID"], $min_piece["Piece ID"], $min, formatDate($min_start),formatDate($min_end));
+if ( $count_down > 0 ) {
+        printf("Number of events: %d, average time: %.3f\n", $count_down, $sum_down/$count_down);
+        printf("Slowest: satellite: %s, piece: %s, time: %.3f, start: %s, end: %s\n", $max_piece["Satellite ID"], $max_piece["Piece ID"], $max, formatDate($max_start),formatDate($max_end));
+        printf("Fastest: satellite: %s, piece: %s, time: %.3f, start: %s, end: %s\n", $min_piece["Satellite ID"], $min_piece["Piece ID"], $min, formatDate($min_start),formatDate($min_end));
+} else {
+        printf("No download events in the log\n");
+}
